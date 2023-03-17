256e90b3d22bc6b4b6a6804a81aaefd2cc8cd0b5
Tobias WEBER <tweber@ill.fr>
2023-03-17 15:07:48 +0100

    forgotten header

--- magnon-plugin/src/magnonmod.cpp
+++ magnon-plugin/src/magnonmod.cpp
@@ -1,8 +1,29 @@
 /**
- * S(q,w) module for magnon dynamics
+ * S(q,w) module for magnetic dynamics
  * @author Tobias Weber <tweber@ill.fr>
  * @date jan-2022
  * @license GPLv2, see 'LICENSE' file
+ *
+ * ----------------------------------------------------------------------------
+ * Takin (inelastic neutron scattering software package)
+ * Copyright (C) 2017-2023  Tobias WEBER (Institut Laue-Langevin (ILL),
+ *                          Grenoble, France).
+ * Copyright (C) 2013-2017  Tobias WEBER (Technische Universitaet Muenchen
+ *                          (TUM), Garching, Germany).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * ----------------------------------------------------------------------------
  */
 
 // g++ -std=c++20 -I../ext -I../ext/takin -I/usr/include/lapacke -Iext/lapacke/include -Lext/lapacke/lib -I/usr/local/opt/lapack/include -L/usr/local/opt/lapack/lib -shared -fPIC -o magnonmod.so magnonmod.cpp ../ext/takin/tools/monteconvo/sqwbase.cpp ../ext/tlibs/log/log.cpp -llapacke
@@ -292,7 +313,7 @@ SqwBase* MagnonMod::shallow_copy() const
 
 std::tuple<std::string, std::string, std::string> sqw_info()
 {
-	return std::make_tuple(TAKIN_VER, "magnonmod", "Magnon Dynamics");
+	return std::make_tuple(TAKIN_VER, "magnonmod", "Magnetic Dynamics");
 }
 
 
