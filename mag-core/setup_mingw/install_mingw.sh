d54b2598e452eed4c48eb34b3a1fd03cf4780ef3
Tobias WEBER <tweber@ill.fr>
2023-06-06 10:44:05 +0200

    setup: updated mingw dependencies

--- mag-core/setup_mingw/install_mingw.sh
+++ mag-core/setup_mingw/install_mingw.sh
@@ -35,22 +35,23 @@ INSTALL_DIR=~/.wine/drive_c/takin2tools
 # binaries
 mkdir -vp ${INSTALL_DIR}
 
-cp -v tools/bz/build/moldyn.exe				${INSTALL_DIR}/
-cp -v tools/magdyn/build/moldyn.exe			${INSTALL_DIR}/
-cp -v tools/moldyn/build/moldyn.exe			${INSTALL_DIR}/
-cp -v tools/pol/build/pol.exe				${INSTALL_DIR}/
-cp -v tools/structfact/build/structfact.exe		${INSTALL_DIR}/
-cp -v tools/magstructfact/build/magstructfact.exe	${INSTALL_DIR}/
+cp -v tools/bz/build/takin_bz.exe			${INSTALL_DIR}/
+cp -v tools/magdyn/build/takin_magdyn.exe		${INSTALL_DIR}/
+cp -v tools/moldyn/build/takin_moldyn.exe		${INSTALL_DIR}/
+cp -v tools/pol/build/takin_pol.exe			${INSTALL_DIR}/
+cp -v tools/structfact/build/takin_structfact.exe	${INSTALL_DIR}/
+cp -v tools/magstructfact/build/takin_magstructfact.exe	${INSTALL_DIR}/
 cp -v tools/cif2xml/build/*.exe				${INSTALL_DIR}/
 
 
 
 # libraries
-MINGW_LIBS="Qt5Core.dll Qt5Gui.dll Qt5Widgets.dll \
+MINGW_LIBS="Qt5Core.dll Qt5Gui.dll Qt5Widgets.dll Qt5Svg.dll \
 	libstdc++-6.dll libglib-2.0-0.dll libgcc_s_seh-1.dll libwinpthread-1.dll \
-	libintl-8.dll iconv.dll libpcre2-16-0.dll libpcre-1.dll \
+	libboost_program_options-mt-x64.dll libqhull_r.dll \
+	libintl-8.dll iconv.dll libpcre2-16-0.dll libpcre2-8-0.dll libpcre-1.dll \
 	zlib1.dll libbz2-1.dll libpng16-16.dll \
-	libharfbuzz-0.dll libfreetype-6.dll"
+	libssp-0.dll libharfbuzz-0.dll libfreetype-6.dll"
 
 for FILE in ${MINGW_LIBS}; do
 	cp -v ${MINGW_DIR}/${FILE} ${INSTALL_DIR}/
@@ -60,7 +61,9 @@ done
 
 # qt libraries
 mkdir -vp ${INSTALL_DIR}/qtplugins/platforms/
+mkdir -vp ${INSTALL_DIR}/qtplugins/styles/
 cp -v ${MINGW_LIB_DIR}/qt5/plugins/platforms/*.dll ${INSTALL_DIR}/qtplugins/platforms/
+cp -v ${MINGW_LIB_DIR}/qt5/plugins/styles/*.dll ${INSTALL_DIR}/qtplugins/styles/
 
 
 
